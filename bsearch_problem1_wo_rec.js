6f308948f65334d9f5c4521c1831e4a5abffb2ae
Sri Harsha Vemparala <sriharshavemparala@gmail.com>
2022-12-01 15:02:54 +0530

    BINARY SEARCH PROBLEM 1 WITHOUT RECURSION

--- bsearch_problem1_wo_rec.js
+++ bsearch_problem1_wo_rec.js
@@ -5,14 +5,15 @@ given array Ai whether the given N is duplicate in the Array or not. If it is du
 output as “FALSE” if it is non duplicate output as “TRUE”.
 */
 
-const arr = [1,2,4,5,5,7,8,9,10];
-const target =5;
+const arr = [1,2,4,5,5,7,7,8,9,10];
+const target =7;
 let start = 0, end = arr.length-1;
 
-//SOLUTION WITH RECURSION
+//SOLUTION WITHOUT RECURSION
 function find_duplicate(arr, start, end, target){
     while(start <= end){
         let mid = Math.floor((start + end)/2);
+
         if(arr[mid] === target){
             if ((arr[mid-1] === target) || (arr[mid+1] === target)){
                 return false;
@@ -22,10 +23,10 @@ function find_duplicate(arr, start, end, target){
             }
         }
         else if(arr[mid] < target){
-            return find_duplicate(arr, mid+1, end, target);            
+            start = mid+1;            
         }
         else {
-            return find_duplicate(arr, start, mid-1, target);            
+            end = mid-1;
         }
     }
 }
