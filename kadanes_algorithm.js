6c29a3584952eccce6ecb68f1483e5922498db0f
Sri Harsha Vemparala <sriharshavemparala@gmail.com>
2022-12-05 20:11:51 +0530

    Kadane's Algorithm

---
+++ kadanes_algorithm.js
@@ -0,0 +1,12 @@
+function maxSubarray(ar) {
+    let pre = ar[0];
+    let ans = Number.MIN_SAFE_INTEGER;
+    for (let i = 1; i < ar.length; i++) {
+        let cur = Math.max(ar[i], pre + ar[i]);
+        pre = cur;
+        ans = Math.max(ans, pre);
+    }
+    console.log(ans);
+}
+
+maxSubarray([-2, -3, 4, -1, -2, 1, 5, -3])
